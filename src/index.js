c1fb6639060ccffcf9837e02e7ae9c3cc46b6ec0
chenxiaodong <chenxiaodong@xiaomi.com>
2017-09-14 22:31:53 +0800

    feat: 支持作用域提升

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import _ from 'lodash';
+import {square} from './math.js';
 function component() {
   var element = document.createElement('div');
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = ['Hello', 'webpack'].join(' ') + square(3);
   return element;
 }
 document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+var webpack = require('webpack');
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin()
+    //new UglifyJSPlugin()
+  ]
+};
