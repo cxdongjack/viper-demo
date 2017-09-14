4b3f473d6697b644959f7e80f09263c0b89b0383
chenxiaodong <chenxiaodong@xiaomi.com>
2017-09-14 23:34:02 +0800

    feat: 增加viperjs模块, 及eslint支持

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// http://eslint.cn/docs/rules/comma-spacing
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env": {
+        "browser": true,
+        "jasmine": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": [
+            "error", {
+                allow: [ "error" ]
+            }
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "space-before-blocks": "error",
+        "comma-spacing": "error"
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import {square} from './math.js';
+import { appendHtml } from 'viperjs/dom';
 function component() {
-  var element = document.createElement('div');
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = ['Hello', 'webpack'].join(' ') + square(3);
-  return element;
+    return ['Hello', 'webpack'].join(' ');
 }
-document.body.appendChild(component());
+appendHtml(document.body, component());
